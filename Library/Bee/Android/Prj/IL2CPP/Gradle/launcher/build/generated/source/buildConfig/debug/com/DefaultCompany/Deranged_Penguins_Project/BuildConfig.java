8609131d5728b9646b23ea4887e8d0a289dcf2e5
Jonathan Gresl <jgresl@hotmail.com>
2024-11-29 02:29:03 -0700

    Added turn provider to locomotion system

---
+++ Library/Bee/Android/Prj/IL2CPP/Gradle/launcher/build/generated/source/buildConfig/debug/com/DefaultCompany/Deranged_Penguins_Project/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.DefaultCompany.Deranged_Penguins_Project;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.DefaultCompany.Deranged_Penguins_Project";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "0.1";
+}
